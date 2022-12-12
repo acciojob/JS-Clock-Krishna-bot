378a0947a92e90c3b077e5e7b6e46f6bb78be94f
Your name <krishnasingh6060@gmail.com>
2022-12-13 01:22:04 +0530

    Final Push

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@
 setInterval(() =>{
     h = new Date ();
     hTime = h.getHours();
-    mTime = h.getMinutes();
+    minTime = h.getMinutes();
     sTime = h.getSeconds();
-    hRotetaion = 30*hTime + mTime/2;
-    mRotetaion = 6*mTime;
+    hRotetaion = 30*hTime + minTime/2;
+    mRotetaion = 6*minTime;
     sRotetaion = 6*sTime;
 
-    handI.style.transform =  `rotate(${hTime}deg)`;
-    handII.style.transform = `rotate(${mTime}deg)` ;
-    handIII.style.transform = `rotate(${sTime}deg)` ;
+    handI.style.transform =  `rotate(${hRotetaion}deg)`;
+    handII.style.transform = `rotate(${mRotetaion}deg)` ;
+    handIII.style.transform = `rotate(${sRotetaion}deg)` ;
 }, 1000);
