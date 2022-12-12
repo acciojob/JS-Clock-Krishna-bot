6248264acbcd20da2dea7a7b9d9483c487e2ba12
Your name <krishnasingh6060@gmail.com>
2022-12-13 01:11:16 +0530

    try1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 //your code here
+setInterval(() =>{
+    h = new Date ();
+    hTime = h.getHours();
+    mTime = h.getMinutes();
+    sTime = h.getSeconds();
+    hRotetaion = 30*hTime + mTime/2;
+    mRotetaion = 6*mTime;
+    sRotetaion = 6*sTime;
+
+    handI.style.transform =  `rotate(${hTime}deg)`;
+    handII.style.transform = `rotate(${mTime}deg)` ;
+    handIII.style.transform = `rotate(${sTime}deg)` ;
+}, 1000);
